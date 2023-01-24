3de769fd64ca5d8b60fa9e0b963a3cc47c88d0e1
vipulb11 <123424699+vipulb11@users.noreply.github.com>
2023-01-25 04:20:21 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region     = "ap-south-1"
-    access_key = "AKIA52EMFTQ6DCCMFHU7"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
   }
 
 resource "aws_s3_bucket" "b" {
